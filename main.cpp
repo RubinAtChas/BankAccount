e4ae2e57ae70ca116dc3a385fe7318acc8c208a3
Rappich <fredrik.martensson@chasacademy.se>
2025-01-13 12:25:08 +0100

    example

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include "include/Bank.hpp"
+
+void clientSimulation(Bank &bank, int clientId)
+{
+    std::srand(static_cast<unsigned int>(std::time(nullptr)) + clientId);
+    for (int i = 0; i < 10; ++i)
+    {
+        int accountNumber = 1000 + std::rand() % 5;
+        int action = std::rand() % 3;
+        int amount = (std::rand() % 100) + 1;
+
+        auto account = bank.getAccount(accountNumber);
+        if (account)
+        {
+            switch (action)
+            {
+            case 0:
+                account->deposit(amount);
+                std::cout << "Client " << clientId << ": Deposited " << amount
+                          << " to account " << accountNumber << std::endl;
+                break;
+            case 1:
+                account->withdraw(amount);
+                std::cout << "Client " << clientId << ": Attempted to withdraw " << amount
+                          << " from account " << accountNumber << std::endl;
+                break;
+            case 2:
+                std::cout << "Client " << clientId << ": Checked balance of account "
+                          << accountNumber << ": " << account->getBalance() << std::endl;
+                break;
+            }
+        }
+        else
+        {
+            std::cout << "Client " << clientId << ": Account " << accountNumber
+                      << " not found." << std::endl;
+        }
+    }
+}
+
+int main()
+{
+    Bank bank;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        bank.addAccount(1000 + i, 1000);
+    }
+
+    std::vector<std::thread> clients;
+    for (int i = 0; i < 10; ++i)
+    {
+        clients.emplace_back(clientSimulation, std::ref(bank), i + 1);
+    }
+
+    for (auto &client : clients)
+    {
+        client.join();
+    }
+
+    std::cout << "\nFinal account balances:\n";
+    bank.displayAllAccounts();
+
+    return 0;
+}
