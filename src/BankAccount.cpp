b79484951f2a0c5a8d563ada0dd0aa46a2c06086
Sand <sandra.obradovic@chasacademy.se>
2025-01-20 10:47:36 +0100

    added mutex lock and sleep_for in BankAccount

--- src/BankAccount.cpp
+++ src/BankAccount.cpp
@@ -1,25 +1,34 @@
 #include "../include/BankAccount.hpp"
+#include <thread>
+#include <chrono>
 
 BankAccount::BankAccount() : balance(0.0), accountNumber(0) {}
 BankAccount::BankAccount(int accountNumber, float balance) : accountNumber(accountNumber), balance(balance) {}
 
 void BankAccount::deposit(float amount)
-{
+{        
+    std::lock_guard<std::mutex> lock(accountMutex);
     if (amount > 0)
     {
         balance += amount;
     }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); //test 
 }
 
 void BankAccount::withdraw(float amount)
 {
+    std::lock_guard<std::mutex> lock(accountMutex);
     if (amount > 0 && amount <= balance)
     {
         balance -= amount;
     }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); //test
 }
 
 float BankAccount::getBalance() const
 {
+    std::lock_guard<std::mutex> lock(accountMutex);
+
     return balance;
 }
+
